8663b29bbe469ed667143ea74d74c95023a384c0
Jonathon <jonathonvanausdal257@gmail.com>
2022-09-06 14:32:54 -0600

    port 3001

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 301;
+const PORT = process.env.PORT || 3001;
 const path = require('path');
 const fs = require('fs');
 const notes = require('./db/notes');
