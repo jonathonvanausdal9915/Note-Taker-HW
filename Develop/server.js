65018a09c2aab4155dc92a7ec177c00ef7c86f7a
Jonathon <jonathonvanausdal257@gmail.com>
2022-09-05 18:30:00 -0600

    get notes app.get to server up the notes from directory

--- Develop/server.js
+++ Develop/server.js
@@ -2,14 +2,26 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 const path = require('path');
+const fs = require('fs');
 
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
-app.post('/api/reviews', (req, res) => {
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+    // Inform the client
+    res.json(`${req.method} request received to get notes`);
+
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
+});
+
+app.post('/api/notes', (req, res) => {
     // Inform the client that their POST request was received
-    res.json(`${req.method} request received to add a review`);
+    res.json(`${req.method} request received to add notes`);
 
     // Log our request to the terminal
     console.info(`${req.method} request received to add a review`);
