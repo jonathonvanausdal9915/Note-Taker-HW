ec5be1dce07528cd022cce584e6ba668b03bce48
Jonathon <jonathonvanausdal257@gmail.com>
2022-09-06 14:21:49 -0600

    setup for heroku

--- Develop/server.js
+++ Develop/server.js
@@ -18,10 +18,38 @@ app.use(express.static('public'));
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request received to get notes`);
     return res.json(notes);
 });
+
+
+// app.delete(`/api/notes/${note_id}`, (req, res) =>
+
+//     fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+//         if (err) {
+//             console.error(err);
+//         } else {
+//             // Convert string into JSON object
+//             const parsedNotes = JSON.parse(data);
+
+//             // Add a new notes
+//             parsedNotes.pop(note_id);
+
+//             // Write updated notes back to the file
+//             fs.writeFile(
+//                 './db/notes.json',
+//                 JSON.stringify(parsedNotes, null, 3),
+//                 (writeErr) =>
+//                 writeErr ?
+//                 console.error(writeErr) :
+//                 console.info('Successfully updated notes!')
+//             );
+//         }
+//     }));
+
+
 
 app.post('/api/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
